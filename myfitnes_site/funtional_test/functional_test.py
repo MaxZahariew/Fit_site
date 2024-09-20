ad028b5384538de4fdaec71ddf3dcac6bc5f2473
Maxim <maxim.zahariew@yandex.ru>
2024-09-20 11:16:29 +0700

    Добавлен функциональный тест для регистрации пользователя

---
+++ myfitnes_site/funtional_test/functional_test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import unittest
+
+
+class NewUserRegisterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_new_user_register_account_in_database(self):
+        self.browser.get('http://localhost:8000/user/register')
+
+        self.assertIn('DJF', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
